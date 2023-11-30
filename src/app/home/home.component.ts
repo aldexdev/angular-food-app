d476bd2f875d78fe333bc3852d76437bf284cfdc
Alejandro Delgado <ialexx9919@gmail.com>
2023-11-30 20:27:59 +0100

    creating-food-cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FoodService } from '../services/food/food.service';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FoodService } from "../services/food/food.service";
+import { Food } from "../shared/models/Food";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent implements OnInit {
-  foods: String[] = [];
+  foods: Food[] = [];
 
   constructor(private foodService: FoodService) {}
 
